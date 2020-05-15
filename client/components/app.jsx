52343e140a8c6e5f89e5bf443be08bd9afe56c1a
AlexanderHeo <jikster712@gmail.com>
2020-05-15 11:44:18 -0700

    1: View All Grades - complete

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import Gradetable from './gradetable';
+import Header from './header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      grades: []
+    });
+  }
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.setState({
+          grades: jsonData
+        });
+      })
+      .catch(err => {
+        console.error('Something went wrong:', err);
+      });
+  }
+
   render() {
-    return null;
+    return (
+      <div className="sgt">
+        <Header />
+        <Gradetable grades={ this.state.grades }/>
+      </div>
+    );
   }
 }
 
---
+++ client/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header"><h1>Student Grade Table</h1></div>
+  );
+}
+
+export default Header;
