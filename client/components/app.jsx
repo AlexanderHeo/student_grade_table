37a222350e5d9c91ac960fe11759513a063e8007
AlexanderHeo <jikster712@gmail.com>
2020-05-15 18:42:51 -0700

    wip - have getAvgGrade inside of compDidMount, need to be its own method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,7 +6,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
-      grades: []
+      grades: [],
+      avgGrade: 0
     });
   }
 
@@ -14,25 +15,49 @@ class App extends React.Component {
     this.getAllGrades();
   }
 
+  // componentDidUpdate() {
+  //   console.log(this.state);
+  // }
+
   getAllGrades() {
     fetch('/api/grades')
       .then(response => {
         return response.json();
       })
       .then(jsonData => {
-        this.setState({
-          grades: jsonData
+        const grades = jsonData;
+        const gradesArr = [];
+        jsonData.map(x => {
+          gradesArr.push(x.grade);
         });
-      })
-      .catch(err => {
-        console.error('Something went wrong:', err);
+        const avg = gradesArr.reduce((acc, cur) => {
+          return acc + cur;
+        });
+        this.setState({
+          grades: jsonData,
+          avgGrade: Math.round(avg / grades.length)
+        });
       });
   }
+
+  // getAverageGrades() {
+  //   const gradesArr = [];
+  //   const mapped = grades.map(x => {
+  //     gradesArr.push(x.grade);
+  //   });
+  //   const avg = gradesArr.reduce((acc, cur) => {
+  //     return acc + cur;
+  //   });
+  //   return avg;
+  // this.setState({
+  //   avgGrade: [length === 0 ? 0 : { avg }]
+  // });
+  // }
 
   render() {
     return (
       <div className="sgt">
-        <Header />
+        <Header avgGrade={ this.state.avgGrade }/>
         <Gradetable grades={ this.state.grades }/>
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
   return (
-    <div className="header"><h1>Student Grade Table</h1></div>
+    <div className="header">
+      <div className="heading">
+        <h1>Student Grade Table</h1>
+      </div>
+      <div className="avgGradeMain">
+        <div className="avgGradeContainer">
+          <div className="avgGradeLabel">
+            <h2>Average Grade:</h2>
+          </div>
+          <div className="gradeBox">
+            <h2>{ props.avgGrade }</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
